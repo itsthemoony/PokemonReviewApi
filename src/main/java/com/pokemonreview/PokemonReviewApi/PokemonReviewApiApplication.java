29c1fd33128c8c30585b5674b23e9094046c1587
Mahan Kheirollahi <kheirollahi.mahan@gmail.com>
2025-07-24 12:43:50 +0300

    initial commit

---
+++ src/main/java/com/pokemonreview/PokemonReviewApi/PokemonReviewApiApplication.java
@@ -0,0 +1,13 @@
+package com.pokemonreview.PokemonReviewApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemonReviewApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokemonReviewApiApplication.class, args);
+	}
+
+}
