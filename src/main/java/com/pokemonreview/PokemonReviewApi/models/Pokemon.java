55907281214d95ac28bd2181e8dfa662bacadfec
Mahan Kheirollahi <kheirollahi.mahan@gmail.com>
2025-07-24 13:10:19 +0300

    feat(model): Add pokemon and review models

---
+++ src/main/java/com/pokemonreview/PokemonReviewApi/models/Pokemon.java
@@ -0,0 +1,9 @@
+package com.pokemonreview.PokemonReviewApi.models;
+import lombok.Data;
+
+@Data
+public class Pokemon {
+    private int id;
+    private String name;
+    private String type;
+}
---
+++ src/main/java/com/pokemonreview/PokemonReviewApi/models/Review.java
@@ -0,0 +1,10 @@
+package com.pokemonreview.PokemonReviewApi.models;
+import lombok.Data;
+
+@Data
+public class Review {
+    private int id;
+    private String title;
+    private String content;
+    private int start;
+}
