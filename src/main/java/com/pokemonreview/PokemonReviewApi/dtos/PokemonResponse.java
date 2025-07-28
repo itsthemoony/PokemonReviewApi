848881d13698db8dced7ab7e7cd366aa5603fbc4
Mahan Kheirollahi <kheirollahi.mahan@gmail.com>
2025-07-28 20:50:49 +0300

    feat(pokemon response): Add pokemon response object to be able to send pokemons by pages

---
+++ src/main/java/com/pokemonreview/PokemonReviewApi/dtos/PokemonResponse.java
@@ -0,0 +1,14 @@
+package com.pokemonreview.PokemonReviewApi.dtos;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class PokemonResponse {
+    private List<PokemonDto> content;
+    private int pageNo;
+    private int pageSize;
+    private long totalElements;
+    private int totalPages;
+    private boolean last;
+}
