3154571d3b1a8fcb181bdaa0de4c73166287bee1
Mahan Kheirollahi <kheirollahi.mahan@gmail.com>
2025-08-04 09:10:28 +0300

    feat(models): Add pokemon and review relationships and Fix start field to stars

--- src/main/java/com/pokemonreview/PokemonReviewApi/models/Review.java
+++ src/main/java/com/pokemonreview/PokemonReviewApi/models/Review.java
@@ -1,8 +1,5 @@
 package com.pokemonreview.PokemonReviewApi.models;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -17,5 +14,10 @@ public class Review {
     private int id;
     private String title;
     private String content;
-    private int start;
+    private int stars;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "pokemon_id")
+    private Pokemon pokemon;
+
 }
